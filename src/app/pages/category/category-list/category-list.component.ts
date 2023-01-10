e54289b31cbf3723b40856adb3d34748414646b2
magefra9 <magefra9@hotmail.com>
2023-01-10 11:00:07 -0600

    Agregando filtro de busqueda por activo o inactivo

--- src/app/pages/category/category-list/category-list.component.ts
+++ src/app/pages/category/category-list/category-list.component.ts
@@ -49,6 +49,26 @@ export class CategoryListComponent implements OnInit {
       return false
   }
 
+  setData(data: any = null){
+    this.component.filters.stateFilter = data.value;
+    this.component.menuOpen = false;
+    this.formatGetInputs();
+  }
+
+  formatGetInputs(){
+    let inputs = {
+      numFilter : 0,
+      textFilter : "",
+      stateFilter: null,
+      starDate: null,
+      endDate: null
+    }
+
+    if(this.component.filters.stateFilter != null){
+      inputs.stateFilter = this.component.filters.stateFilter
+    }
+    this.component.getInputs = inputs;
+  }
 
   CategoryRemove(row: CategoryApi) {
     
