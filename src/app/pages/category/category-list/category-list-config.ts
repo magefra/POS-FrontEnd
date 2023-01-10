dbdcd77f15d7063580cdb0199d4777a4a297312c
magefra9 <magefra9@hotmail.com>
2023-01-10 10:19:43 -0600

    Agregando parametros de filtros en configuración

--- src/app/pages/category/category-list/category-list-config.ts
+++ src/app/pages/category/category-list/category-list-config.ts
@@ -1,6 +1,38 @@
 import { Category } from 'src/app/responses/category/category.response';
 import { TableColumn } from '../../../../@vex/interfaces/table-column.interface';
 import icCategory from "@iconify/icons-ic/twotone-category"
+import { ListTableMenu } from '../../../commons/list-table-menu.interface';
+import icViewHeadline from "@iconify/icons-ic/twotone-view-headline"
+import icLabel from "@iconify/icons-ic/twotone-label"
+
+const menuItems:  ListTableMenu[] = [
+  {
+    type: "link",
+    id: "all",
+    icon: icViewHeadline,
+    label: "Todos",
+  },
+  {
+    type: "link",
+    id: "Activo",
+    value: 1,
+    icon: icLabel,
+    label: "Activo",
+    classes: {
+      icon: "text-green"
+    }
+  },
+  {
+    type: "link",
+    id: "Inactivo",
+    value: 0,
+    icon: icLabel,
+    label: "Inactivo",
+    classes: {
+      icon: "text-gray"
+    }
+  },
+]
 
 const tableColumns : TableColumn<Category>[] =[
   {
@@ -49,6 +81,14 @@ const tableColumns : TableColumn<Category>[] =[
   }
 ];
 
+const filters = {
+  numFilter: 0,
+  textFilter: "",
+  stateFilter : null,
+  startDate: null,
+  endDate: null
+}
+
 const inputs = {
   numFilter: 0,
   textFilter: "",
@@ -61,12 +101,15 @@ const inputs = {
 export const componentSettings = {
     //icons
     icCategory:  icCategory,
+    menuOpen: false,
     tableColumns: tableColumns,
     initialSort: "Id",
     initialSortDir: "desc",
     getInputs: inputs,
     buttonLabel: "EDITAR",
     buttonLabel2: "ELIMINAR",
+    menuItems: menuItems,
+    filters: filters,
     columnsFilter: tableColumns.map((column) => { return {label: column.label, property: column.property, type: column.type}})
 
 
